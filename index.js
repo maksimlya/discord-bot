d4ea8ec36a06686276aa94c400dd8b7aea12870d
maksimlya <maksimlya@gmail.com>
2021-04-15 01:56:22 +0300

    Added base modules.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!!!');
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Bot is ready');
+  });
+
+  client.login(process.env.BOT_TOKEN);
